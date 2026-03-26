dad76ede0fc422ff40c22ee319a34148626a33b5
Yang Chi <3136089844@qq.com>
2026-03-26 16:05:42 +0800

    Exercise 7

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        UserRegistration user = new UserRegistration();
-        user.registration();
-        System.out.println(user);
+        AdminPanel adminPanel = new AdminPanel();
+        adminPanel.userManagementOptions();
     }
 }
